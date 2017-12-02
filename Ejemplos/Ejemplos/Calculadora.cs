c35f3a9e819c4bd75b31371a670df9a14c2ddee8
LOLO\Pc <rafael_v56@hotmail.com>
2017-12-02 01:47:55 +0100

    Agregado el repositorio, con algunos ejemplos de código

---
+++ Ejemplos/Ejemplos/Calculadora.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplos
+{
+    class Calculadora
+    {
+        static void Main(string[] args)
+        {
+            Calculos calculadora = new Calculos();
+
+            calculadora.Menu();
+        }
+    }
+
+    class Calculos
+    {
+        double total;
+        double[] numeros;
+        public Calculos()
+        {
+            numeros = new double[2]; 
+        }
+
+        public void Menu()
+        {
+            int eleccion;
+            do
+            {
+                Console.WriteLine("Calculadora\n");
+                Console.Write("1)Suma\n2)Resta\n\n0)Salir\n-->");
+                try
+                {
+                    eleccion = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    eleccion = -1;                    
+                }
+                if (eleccion!=0)
+                {
+                    total = 0;
+                    
+                    switch (eleccion)
+                    {
+                        case 1:
+                            PedirNumeros();
+                            total = Suma(numeros[0], numeros[1]);
+                            Console.WriteLine("La suma es: "+total);
+                            break;
+                        case 2:
+                            PedirNumeros();
+                            total = Resta(numeros[0], numeros[1]);
+                            Console.WriteLine("La resta es: " + total);
+                            break;
+                        default:
+
+                            Console.WriteLine("No es una elección correcta\n");
+                            break;
+                    }
+                }
+            } while (eleccion!=0);
+        }
+
+        private double Resta(double v1, double v2)
+        {
+            return v1 - v2;
+        }
+
+        private void PedirNumeros()
+        {
+            Console.WriteLine("Dame el numero uno");
+            numeros[0] = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Dame el numero dos");
+            numeros[1] = Convert.ToDouble(Console.ReadLine());
+        }
+
+        private double Suma(double uno,double dos)
+        {
+            return uno+dos;
+        }
+
+        //Destructor de la clase
+        ~Calculos()
+        {
+            Console.WriteLine("Final de los cálculos");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejemplos/Ejemplos/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplos
+{
+    class Program
+    {
+        static void Mai(string[] args)
+        {
+            //Entero
+            int entero = 22;
+
+            //Real
+            double real = 22.5;
+
+            //Lógico o Boleano
+            bool boleano = true;
+
+            //Caracter
+            char caracter = 'ñ';
+
+            //Cadena de texto
+            string cadena = "cadena de texto";
+
+            //Real corto
+            float realCorto = 23.4332f;
+
+            //Entero largo
+            long enteroLargo = 2123123123;
+
+            //Variable que debe ser inicializada con un valor, o dará error, se asume el valor que se le asigne
+            var cualquiera = "hola";
+
+            //debe ser inicializada con un valor, puede cambiar de tipo
+            dynamic dinamica = "Otra variable";
+            dinamica = 23;
+
+
+            //Constantes. Debe ser inicializada con un valor cuando son declaradas
+            const double PI = 3.14;
+
+            //Escribe en pantalla con un salto de línea
+            Console.WriteLine(dinamica);
+
+            //Entrada de usuario, retorna un string
+            Console.ReadLine();
+
+            //Convertir un tipo a otro Convert.TOXXXX
+            int otroEntero = Convert.ToInt32(dinamica);
+            Console.WriteLine(otroEntero);
+
+            //División
+            //Si los operandos son enteros cualquier resto es omitido para retornar un valor entero.
+            int division = 10 / 4;
+            Console.WriteLine(division);//Retorna 2
+
+            //Módulo o resto
+            //retorna el resto de una división entera
+            int resto = 25 % 7;
+            Console.WriteLine(resto);//retorna 4
+
+            //Operadores de asignación
+            int x = 2;
+            x += 2;
+            x -= 3;
+            Console.WriteLine(x);
+
+
+            //Operadores realcioneles
+            /*
+             * >=  7 >= 4 Verdadero
+             * <=  7 <= 4 Falso
+             * ==  7 >= 4 Falso
+             * !=  7 >= 4 Verdadero
+             * */
+
+
+            //Operadores aritméticos
+            //Igual que en java
+
+
+            //Operadores lógicos
+            //igual que en java  && || !
+
+            //Operador ternario o condicional
+            //igual que en java ? :
+
+
+
+            //CONTROL DE FLUJO
+            //iF ELSE
+            if (3 < 4)
+            {
+                Console.WriteLine("if ");
+            }
+
+            if (5 < 4)
+            {
+            }
+            else
+            {
+                Console.WriteLine("if else ");
+            }
+
+            //Switch
+            int num = 3;
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("switch 1");
+                    break;
+                case 2:
+                    Console.WriteLine("switch 2");
+                    break;
+                case 3:
+                    Console.WriteLine("switch 3");
+                    break;
+                default:
+                    Console.WriteLine("switch default");
+                    break;
+            }
+
+            //While
+            int num2 = 1;
+            while (num2 < 6)
+            {
+                Console.WriteLine(num2);
+                num2++;
+            }
+
+            //for
+            for (int i = 10; i < 15; i++) //for (int i = 10; i < 15; i+=3)  otro ejemplo
+            {
+                Console.WriteLine(i);
+            }
+
+            //Do while
+
+            do
+            {
+                Console.WriteLine(num2);
+                num2++;
+            } while (num2>333);
+
+
+
+
+            Console.ReadKey();//lee solo un caracter
+        }
+    }
+}
